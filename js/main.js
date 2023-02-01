16296598b390f258dffc4ddb5fc90a68904bbfb1
marcin <brand.marcin@gmail.com>
2023-02-01 17:57:16 +0000

    zadanie43671

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let s1 = "Uwielbiam JavaScript";
+let s2 = "Jestem świetnym programistą";
+
+
+
+
+
+// console.log(`${s1} ${s1.length}`);
+// console.log(`${s2} ${s2.length}`);
+// console.log(s1.length < s2.length);
+
+
+function ls(s1, s2) {
+    if (s1.length > s2.length) {
+        console.log('Uwielbiam JavaScript');
+    }
+    else {
+        (s1.length < s2.length)
+        console.log('Jestem świetnym programistą');
+    }
+
+}
+console.log(ls(s1, s2));
+
+
